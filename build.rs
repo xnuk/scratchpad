91356d0f0ff9fa61679dc0103f36670ff222987e
Xnuk Shuman <admin@xnu.kr>
2021-10-09 12:04:21 +0900

    멍청

--- build.rs
+++ build.rs
@@ -1,6 +1,8 @@
 use bindgen::{Builder, CargoCallbacks};
 use std::{env::var_os, error::Error, path::PathBuf};
 
+const LIBMSI_REGEX: &'static str = "(?i).*libmsi.*";
+
 fn main() -> Result<(), Box<dyn Error>> {
 	println!("cargo:rustc-link-lib=dylib=msi");
 	println!("cargo:rerun-if-changed=./src/wrapper.h");
@@ -10,9 +12,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 		.clang_arg("--include-directory=/usr/lib/glib-2.0/include/")
 		.clang_arg("--include-directory=/usr/include/glib-2.0/")
 		.clang_arg("--include-directory=/usr/include/libmsi-1.0/")
-		.allowlist_function(".*[lL][iI][bB][mM][sS][iI].*")
-		.allowlist_type(".*[lL][iI][bB][mM][sS][iI].*")
-		.allowlist_var(".*[lL][iI][bB][mM][sS][iI].*")
+		.allowlist_function(LIBMSI_REGEX)
+		.allowlist_type(LIBMSI_REGEX)
+		.allowlist_var(LIBMSI_REGEX)
 		.parse_callbacks(Box::new(CargoCallbacks))
 		.generate()
 		.expect("Unable to generate bindings");
